4d9a8fe3b3c6c1397c9d8009327e2dda1e8ba665
apocalypse575 <apocalypse575@gmail.com>
2017-09-23 19:32:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function happyHolidaysTo(name) {
 
 
 
-function happyHolidayTo(holiday, name) {
+functions happyHolidayTo(holiday, name) {
   return 'Happy ${holiday}, ${name}!'
 }
 console.log(happyHolidayTo('Independence Day', 'you'))
