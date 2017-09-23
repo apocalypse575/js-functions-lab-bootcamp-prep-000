e757c9ca69c7239eb57241dd8f14bb94beb8e982
apocalypse575 <apocalypse575@gmail.com>
2017-09-23 19:30:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
-console.log(happyHolidaysTo('you'))
+
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!"
+}
+console.log(happyHolidayTo('Independence Day', 'you'))
+
+function holidayCountdown(holiday, days) {
+  return "It's ${days} days until ${holiday}!"
+}
+console.log(holidayCountdown('Mother\'s Day', '20'))
